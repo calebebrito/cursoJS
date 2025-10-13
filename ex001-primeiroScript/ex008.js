0dd379c64d6c6f94579f5f8c467edcc360f07bc3
Calebe Ribeiro <ribeirozx15@gmail.com>
2025-10-13 14:37:10 -0300

    CursoJS 1.3

---
+++ ex001-primeiroScript/ex008.js
@@ -0,0 +1,8 @@
+var vel = 80
+console.log(`Your car's speed is ${vel}Km/h`)
+
+if(vel > 60) {
+    console.log(`You exceeded the speed limit. Fined!`)
+}else {
+    console.log(`Always drive using a seat belt`)
+}
